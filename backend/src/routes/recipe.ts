e9f9cfd71a3d9edf4aa8447a491f59175efc8647
Adam Curtis <kallisti.dev@gmail.com>
2022-06-03 08:25:40 -0400

    add route handler for GET /api/recipe/:id

--- backend/src/routes/recipe.ts
+++ backend/src/routes/recipe.ts
@@ -1,9 +1,39 @@
 import { Request, Response, NextFunction } from "express"
+import * as mongoose from 'mongoose';
+import { RecipeModel } from "../models"
+
+const { ObjectId } = mongoose.Types
+
+//TODO: generic API error handler middleware/utilities for 404, 400, 500, etc 
 
 export const recipeMiddleware = async (
   req: Request,
   res: Response,
   next: NextFunction
 ): Promise<void> => {
-  // TODO fetch and return a recipe
+  // get request params
+  const { id } = req.params
+  // validate params
+  if(!id || !ObjectId.isValid(id)) {
+    res.status(400).json({
+      error: `Bad Recipe ID ${id}`,
+      code: 400
+    })
+    return
+  }
+  // fetch recipe from mongo by ID
+  // TODO: catch mongoose error with generic middleware handler and respond with 500 and JSON error response
+  const recipe = await RecipeModel.findById(id);
+  
+  // return recipe as JSON if found
+  if (recipe) {
+    res.json(recipe);
+  }
+  // return 404 if not found
+  else { 
+    res.status(404).json({
+      error: `Recipe ID ${id} not found`,
+      code: 404
+    })
+  }
 }
